77989197d44d285ede10ee549667a1a890941814
Drianos <drianhacon173@gmail.com>
2017-06-09 10:53:31 -0600

    test with firmata.

--- api/controllers/ActuatorsController.js
+++ api/controllers/ActuatorsController.js
@@ -6,17 +6,39 @@
  */
 var net = require('net');
 
+var VirtualSerialPort = require('udp-serial').SerialPort;
+var firmata = require('firmata');
+var five = require("johnny-five");
+var sp = new VirtualSerialPort({
+  host: '192.168.0.117' //make sure to change the ip(use 192.168.4.1 if you are using ESP8266 as Access Point)
+});
+
+var ioboard = new firmata.Board(sp);
+var board = new five.Board({io: ioboard, repl: true});
+
+
 module.exports = {
   turnOnLed : function(req, res)
   {
     var newLigthValue = req.param('intensity')
-	console.log('Light On');
-    var client = new net.Socket();
+	   console.log('Light On');
+    /*var client = new net.Socket();
     client.connect(23, '192.168.100.177', function() {
     	console.log('Connected');
     	client.write(newLigthValue);
-    	client.destroy();
-    });
+    	client.destroy();*/
+
+    var write = (message) => {
+      this.i2cWrite(0x04, Array.from(message, c => c.charCodeAt(0)));
+    };
+    this.i2cConfig();
+    this.repl.inject({ write });
+
+
+    var message1 = String.fromCharCode((newLigthValue.charCodeAt(0) -100)* 10);
+    var message = String.fromCharCode(11);
+    write(message1);
+    write(message2);
 
      //return res.send('On Led');
      return;
